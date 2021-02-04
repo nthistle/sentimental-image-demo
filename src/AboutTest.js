f9f1a57bbaf6f69e77860ebb5baee8e5bbd16e85
pli93 <pli93@gatech.edu>
2021-02-04 10:48:31 -0500

    Added navbar

---
+++ src/AboutTest.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import emptyImage from './empty_image.png'
+import patrickImage from './patricks_favorite_line_graph.png'
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Image from 'react-bootstrap/Image';
+import Modal from 'react-bootstrap/Modal';
+
+class AboutTest extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <p> Hey FeelsOkayMan </p>
+    );
+  }
+}
+
+export default AboutTest;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AboutTest from './AboutTest';
+import Header from './Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={AboutTest} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
